b4e255a28239766cb4da03024b9c55427e921aa7
Don Aitken <don.aitken@gmail.com>
2019-06-17 21:19:13 -0400

    Create initial routes

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+'use strict'
+
+const quotes = require('./quotes.json')
 const express = require('express')
+
 const quotesApi = express()
 
 const port = 8080
 
-quotesApi.get('/', function (req, res) {
-  res.send('Hello World!')
+quotesApi.get('/quotes', function (req, res) {
+  const summary = ({ name, slug }) => ({ name, slug })
+  const cmp = (a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)
+  res.json(quotes.map(q => summary(q)).sort(cmp))
+})
+
+quotesApi.get('/quotes/:slug', function (req, res) {
+  const result = quotes.filter(q => q.slug === req.params['slug'])
+  res.json(result[0].quotes)
 })
 
 quotesApi.listen(port, () =>
